e7c1f792a59f4e472fdb37033af8de4b6eb4fb9f
ganganlee <ganganlee@outlook.com>
2020-04-27 08:32:23 +0800

    完成商品的增删改查

--- README.md
+++ README.md
@@ -10,3 +10,80 @@
 | num | true | uint | 商品数量 |
 |image|true|string|商品图片|
 |url|true|string|商品链接|
+```cgo
+//success
+{
+  "code": 200,
+  "data": {
+    "id": 1
+  },
+  "msg": "ok"
+}
+```
+
+> 2、修改商品
+- /product
+- put
+- json
+
+| 名称 | 必填 | 类型 | 说明 |
+| ------ | ------ | ------ |------ |
+| id | true | int | 商品id |
+| name | true | string | 商品名称 |
+| num | true | uint | 商品数量 |
+|image|true|string|商品图片|
+|url|true|string|商品链接|
+```cgo
+//success
+{
+  "code": 200,
+  "data": {
+    "id": 1
+  },
+  "msg": "ok"
+}
+```
+
+> 3、删除商品
+- /product
+- delete
+- json
+
+| 名称 | 必填 | 类型 | 说明 |
+| ------ | ------ | ------ |------ |
+| id | true | int | 商品id |
+```cgo
+//success
+{
+  "code": 200,
+  "data": {
+    "id": 1
+  },
+  "msg": "ok"
+}
+```
+
+> 4、获取商品列表
+- /product
+- get
+- json
+```cgo
+//success
+{
+  "code": 200,
+  "data": [
+    {
+      "ID": 2,
+      "CreatedAt": "2020-04-27T08:23:45+08:00",
+      "UpdatedAt": "2020-04-27T08:23:45+08:00",
+      "DeletedAt": null,
+      "name": "添加项目添加项目001",
+      "num": 100,
+      "image": "http://www.baidu.com",
+      "url": "http://www.baidu.com"
+    },
+    ...
+  ],
+  "msg": "ok"
+}
+```
--- controller/product.go
+++ controller/product.go
@@ -4,35 +4,40 @@ import (
 	"encoding/json"
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
-	"net/http"
 	. "product.zozoo.net/Server"
 	. "product.zozoo.net/common"
 	"product.zozoo.net/library"
 	"product.zozoo.net/model"
+	"strconv"
 )
 
 //获取所有商品列表
 func GetAll(c *gin.Context) {
-	//获取所有数据
-	//var products = []model.Product{}
-	//Server.Db.Find(products)
-	/**
-	c.HTML(http.StatusOK,"product.html",gin.H{
-		"msg":"ok",
-	})*/
-	c.JSON(http.StatusOK, gin.H{
-		"msg":        "ok",
-		"controller": "get",
-	})
+	var product []model.Product
+	Db.Find(&product)
+	ResponseSuccess(c,product,"ok")
 }
 
 //修改商品
 func Update(c *gin.Context) {
-	//var product = new(model.Product)
-	c.JSON(http.StatusOK, gin.H{
-		"msg":        "ok",
-		"controller": "update",
-	})
+	var product = new(model.Product)
+	//模型绑定获取参数
+	err := c.ShouldBind(product)
+	if err != nil {
+		ResponseError(c, "", err.Error())
+		return
+	}
+
+	//判断项目id是否存在
+	id,err := strconv.Atoi(c.PostForm("id"))
+	if err != nil || id < 0{
+		ResponseError(c, "", "参数错误")
+		return
+	}
+
+	//修改
+	Db.Model(product).Where("id = ?",id).Update(map[string]interface{}{"name": product.Name, "num": product.Num, "image": product.Image,"url":product.Url})
+	ResponseSuccess(c, gin.H{"id": id}, "ok")
 }
 
 //新增商品
@@ -64,9 +69,13 @@ func Insert(c *gin.Context) {
 
 //删除商品
 func Delete(c *gin.Context) {
-	//var product = new(model.Product)
-	c.JSON(http.StatusOK, gin.H{
-		"msg":        "ok",
-		"controller": "delete",
-	})
+	var product = new(model.Product)
+	id,err := strconv.Atoi(c.PostForm("id"))
+	if err != nil || id < 0{
+		ResponseError(c,"","参数错误")
+		return
+	}
+	//删除
+	Db.Unscoped().Where("id = ?",id).Delete(product)
+	ResponseSuccess(c,gin.H{"id":id},"ok")
 }
